7ad4e8e27cb740d977bce2f3521b334285b2bf52
Kirubel <kirubelgirmay21@gmail.com>
2022-09-01 21:50:46 +0300

    cors is added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-
+const cors = require("cors");
 const userroute = require("./api/users");
 const authroute = require("./api/authroute");
 const surveyroute = require("./api/surveyroute");
@@ -15,6 +15,10 @@ dotenv.config({ path: "../config.env" });
 
 const app = express();
 app.use(express.json());
+app.use(cors({ 
+  origin: "https://survey-react-project.herokuapp.com/", 
+  credentials: true 
+ }));
 console.log(process.env.DATABASE_LOCAL);
 
 app.use("/api/user", userroute);
@@ -29,7 +33,7 @@ app.use("/api/deletesurvey", deletesurvey);
 app.use("/", (req, res) => res.send("helow this is from the root directory"));
 
 mongoose
-  .connect(process.env.DATABASE_ATLAS, {
+  .connect(process.env.DATABASE_LOCAL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: true,
@@ -37,8 +41,6 @@ mongoose
   })
   .then(() => {
     console.log("the database connection is successful");
-  }).catch(err => {
-    console.log("the error is ", err)
   });
 
 app.listen(process.env.PORT, () => {
